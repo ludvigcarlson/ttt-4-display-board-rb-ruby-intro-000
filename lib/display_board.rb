9aed20a477aa76ef5a1d59ddb08d826e73633c2a
ludvig.goliath.carlson <ludvig.goliath.carlson@gmail.com>
2018-09-17 19:53:17 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,3 +3,5 @@ def display_board(one, two, three, four, five, six, seven, eight, nine)
 end
 display_board("   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ")
 display_board("   ", "   ", "   ", "   ", " X ", "   ", "   ", "   ", "   ")
+display_board(" X ", " X ", " X ", " X ", " X ", " X ", " X ", " X ", " X ")
+display_board(" O ", " O ", " O ", " O ", " O ", " O ", " O ", " O ", " O ")
