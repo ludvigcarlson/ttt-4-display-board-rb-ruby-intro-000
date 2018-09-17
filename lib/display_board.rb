daf79aec53c845b9de899d9cdebe40d1dc9b67a7
ludvig.goliath.carlson <ludvig.goliath.carlson@gmail.com>
2018-09-17 19:50:37 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,3 +2,4 @@ def display_board(one, two, three, four, five, six, seven, eight, nine)
   puts "#{one}" "|" "#{two}" "|" "#{three}" , "---------" , "#{four}" "|" "#{five}" "|" "#{six}" , "-----------" , "#{seven}" "|" "#{eight}" "|" "#{nine}"
 end
 display_board("   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ")
+display_board("   ", "   ", "   ", "   ", " X ", "   ", "   ", "   ", "   ")
